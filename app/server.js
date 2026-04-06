d7cc674c8bedd5349a7480ed785fd8ec3136cb11
danny.brabant <info@dannybrabant.com>
2026-04-05 20:56:36 -0400

    modif et ajour

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "c",
+  dev: "Hello de l'environnement de DEV",
   qa: "Hello de l'environnement de QA",
   prod: "Hello de l'environnement de PROD",
 };
