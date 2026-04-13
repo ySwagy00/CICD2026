7778eb08937bb623fa5af5c8bd68137555042465
ySwagy00 <6256003@cegepmontpetit.ca>
2026-04-13 15:55:11 -0400

    update message hola

--- app/server.js
+++ app/server.js
@@ -4,9 +4,9 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Salut de l'environnement de DEV, je suis Zakaria",
-  qa: "Salut de l'environnement de QA",
-  prod: "Salut de l'environnement de PROD",
+  dev: "Hola de l'environnement de DEV, je suis Zakaria",
+  qa: "Hola de l'environnement de QA",
+  prod: "Hola de l'environnement de PROD",
 };
 
 const message = messages[ENV] || 'Hello World';
