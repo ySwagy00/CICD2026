d9b44c5f91671e1187488d2e008926ee1c7a178c
ySwagy00 <6256003@cegepmontpetit.ca>
2026-04-24 18:12:19 -0400

    Dan

--- app/server.js
+++ app/server.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3000;
 const ENV = process.env.APP_ENV || 'dev';
 
 const messages = {
-  dev: "Salut, de l'ekip Zakaria sur DEV",
+  dev: "Salut, Dan",
   qa: "Bonjour d'environnement de QA",
   prod: "Salut l'ekip d'environnement de PROD",
 };
